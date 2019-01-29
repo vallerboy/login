81f9c635eda1d96ebdfd62be9ac8af4107daf9c8
Oskix <oskar@boxly.pl>
2019-01-29 19:50:20 +0100

    asd

--- src/main/java/pl/oskarpolak/login/controllers/RegisterController.java
+++ src/main/java/pl/oskarpolak/login/controllers/RegisterController.java
@@ -1,13 +1,23 @@
 package pl.oskarpolak.login.controllers;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import pl.oskarpolak.login.models.services.UserService;
 
 @Controller
 public class RegisterController {
+
+
+    final UserService userService;
+
+    @Autowired
+    public RegisterController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/register")
     public String login() {
@@ -19,7 +29,9 @@ public class RegisterController {
                         @RequestParam("password") String password,
                         @RequestParam("email") String email
                         ){
-        //todo tutaj logika rejestrowania sie (serwis)
-        return "register";
+        if (!userService.addUser(login, password, email)) {
+            //nie udalo sie zarejestrowac
+        }
+        return "redirect:/login"; // udalo sie
     }
 }
